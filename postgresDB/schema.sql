768fd59add4e8ad01bad115ea949062b84fb48a4
maherDamouni <damounim1.gmail.com>
2022-04-22 06:44:34 -0700

    added ETL code in schema

--- postgresDB/schema.sql
+++ postgresDB/schema.sql
@@ -1,17 +1,10 @@
--- ---
--- Globals
--- ---
-
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS Answers;
+DROP TABLE IF EXISTS Questions;
+DROP TABLE IF EXISTS Photos;
 
 -- ---
 -- Table 'Answers'
 -- ---
-
-DROP TABLE IF EXISTS Answers;
-DROP TABLE IF EXISTS Questions;
-DROP TABLE IF EXISTS Photos;
 
 CREATE TABLE Answers (
   id SERIAL NOT NULL,
@@ -24,6 +17,10 @@ CREATE TABLE Answers (
   helpfulness INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+CREATE INDEX Answers_index ON Answers (id);
+CREATE INDEX Answers_Questions_index ON Answers (question_id);
+COPY Public.Answers FROM '/Users/maherdamouni/Desktop/HackReactor/SDC/API-QA/answers.csv' DELIMITER ',' CSV HEADER;
 
 -- ---
 -- Table 'Questions'
@@ -41,6 +38,10 @@ CREATE TABLE Questions (
   PRIMARY KEY (question_id)
 );
 
+CREATE INDEX Questions_index ON Questions (question_id);
+CREATE INDEX Product_index ON Questions (product_id);
+COPY Public.Questions FROM '/Users/maherdamouni/Desktop/HackReactor/SDC/API-QA/questions.csv' DELIMITER ',' CSV HEADER;
+
 -- ---
 -- Table 'Photos'
 -- ---
@@ -51,6 +52,10 @@ CREATE TABLE Photos (
   photo_url VARCHAR(200) NOT NULL,
   PRIMARY KEY (photo_id)
 );
+
+CREATE INDEX Photo_index ON Photos (photo_id);
+CREATE INDEX Photo_Answers_index ON Photos (answer_id);
+COPY Public.Photos FROM '/Users/maherdamouni/Desktop/HackReactor/SDC/API-QA/answers_photos.csv' DELIMITER ',' CSV HEADER;
 
 -- ---
 -- Foreign Keys
